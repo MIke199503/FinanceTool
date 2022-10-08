08b88b11fd87342378916f810e5138cc00aff94d
Mike19950326 <zth19950326@yahoo.com>
2022-10-08 11:02:10 +0800

    完善数据整理

---
+++ DataModule/QueryClass.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Project ：FinanceTool
+@File    ：QueryClass.py
+@Author  ：朱桃禾 MikePy
+@Date    ：2022/10/8 10:52 AM 
+"""
+
+
+class Query_Module:
+    def __init__(self, data):
+        self.data = data
+
+    def query(self, date, company, depart, ):
+        pass
--- DataModule/ReadExcel.py
+++ DataModule/ReadExcel.py
@@ -1,62 +1,159 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Project ：FinanceTool
+@File    ：ReadExcel.py
+@Author  ：朱桃禾 MikePy
+@Date    ：2022/10/8 10:47 AM
+"""
+
 import openpyxl
 import BasicMessage
-import pandas as pd
 from openpyxl.utils import get_column_letter
-import GetDepartment
+
 
 class FirstDeal:
     def __init__(self, filepath):
         self.file_path = filepath
         self.company_sheet_name_dict = {}
+        self.time_data = set()  # 可选日期
 
         # 尝试打开文件，得到wb
         try:
             self.wb = openpyxl.load_workbook(filepath)
             self.workbook_sheets_names = self.wb.sheetnames
-            self.return_data = self.read_single_sheet_table("之了成都教育2206")
-            testa = GetDepartment.GetDepart(self.return_data)
-            testa.get_one_leval()
         except:
             print("somethings wrong")
-
         self.deal_sheets()
+        self.get_time_data()
 
     def deal_sheets(self):
         """
         处理文件相关sheets name 数据规整
         company_sheet_name_dict  :  各公司简写名字的对应有的表名
-        :return: None
+        :return: self.company_sheet_name_dict
         """
-        company_abb = list(BasicMessage.company_abbreviation.values())
-        # company_all = list(BasicMessage.company_abbreviation.keys())
 
+        # 公司简写
+        company_abb = list(BasicMessage.company_abbreviation.values())
+        # company_all = list(BasicMessage.company_abbreviation.keys())  #全写
+
+        # 构建公司字典
         for x in company_abb:
             if x not in self.company_sheet_name_dict:
                 self.company_sheet_name_dict[x] = {}
             else:
                 pass
-
-        for item_company_abb in company_abb:
+        # 读取所有表格中所有的信息，并按照预定格式组合
+        for item_company_abb in company_abb:  #
             for item_sheet_name in self.workbook_sheets_names:
-                if item_company_abb in item_sheet_name:
-                    sheet_data = pd.read_excel(self.file_path, sheet_name=item_sheet_name)
-                    self.company_sheet_name_dict[item_company_abb][item_sheet_name] = sheet_data
-
-        print(self.company_sheet_name_dict)
+                if item_company_abb in item_sheet_name:  # 找到对应表
+                    # 读取单张表格 获取未经处理的所有数据
+                    sheet_data = self.read_single_sheet_table(sheet_name=item_sheet_name)
+                    # 处理数据，得到组合数据
+                    finally_sheet_data = self.rebuilt_page_data(sheet_data)
+                    # 组合数据
+                    self.company_sheet_name_dict[item_company_abb][item_sheet_name] = finally_sheet_data
+        # 可接受返回的数据，也可以直接调取属性
+        return self.company_sheet_name_dict
 
     def read_single_sheet_table(self, sheet_name):
+        """
+        获取单张表格的所有数据
+        :param sheet_name:
+        :return: data
+        """
         ws = self.wb[sheet_name]
         max_row_num = ws.max_row
         max_col_num = ws.max_column
         data = []
-        for row in range(2, max_row_num+1):
+        for row in range(2, max_row_num + 1):
             row_item = []
             for col in range(1, max_col_num):
                 col_letter = get_column_letter(col)
-                row_item.append(ws[col_letter+str(row)].value)
+                row_item.append(ws[col_letter + str(row)].value)
             data.append(row_item)
         return data
 
+    def rebuilt_page_data(self, data_list):
+        """
+        组装单张表格里面的所有数据
+        page_data = {
+            "Leval1": {"code": [], "data": []},
+            "Leval2": {"code": [], "data": []},
+            "Leval3": {"code": [], "data": []},
+            "Leval4": {"code": [], "data": []},
+            "Leval5": {"code": [], "data": []},
+             }
+        :return: page_data
+        """
+        # 预设数据
+        page_data = {
+                    "Leval1": {"code": [], "data": []},
+                    "Leval2": {"code": [], "data": []},
+                    "Leval3": {"code": [], "data": []},
+                    "Leval4": {"code": [], "data": []},
+                    "Leval5": {"code": [], "data": []},
+                     }
+        # 有效一级标题
+        useful_leval_1 = ["6601", "6602", "6603"]
+
+        for data_index in data_list:
+            # 遍历所有数据
+            if data_index is None or data_index[0] is None:
+                # 无效数据第一批次就跳过
+                pass
+            else:
+                for useful_leval in useful_leval_1:
+                    # 只针对有效一级标题下的所有数据
+                    if useful_leval in data_index[0]:
+                        # 有效数据
+
+                        # 组织一级标题及数据
+                        if len(data_index[0]) == 4 and data_index[2] is None:
+                            page_data["Leval1"]["code"].append(data_index[0])
+                        elif len(data_index[0]) == 4 and data_index[2] is not None:
+                            page_data["Leval1"]["data"].append(data_index)
+
+                        # 组织二级
+                        elif len(data_index[0]) == 7 and data_index[2] is None:
+                            page_data["Leval2"]["code"].append(data_index[0])
+                        elif len(data_index[0]) == 7 and data_index[2] is not None:
+                            page_data["Leval2"]["data"].append(data_index)
+
+                        # 组织三级
+                        elif len(data_index[0]) == 10 and data_index[2] is None:
+                            page_data["Leval3"]["code"].append(data_index[0])
+                        elif len(data_index[0]) == 10 and data_index[2] is not None:
+                            page_data["Leval3"]["data"].append(data_index)
+
+                        # 组织四级
+                        elif len(data_index[0]) == 13 and data_index[2] is None:
+                            page_data["Leval4"]["code"].append(data_index[0])
+                        elif len(data_index[0]) == 13 and data_index[2] is not None:
+                            page_data["Leval4"]["data"].append(data_index)
+
+                        # 组织五级
+                        elif len(data_index[0]) == 16 and data_index[2] is None:
+                            page_data["Leval5"]["code"].append(data_index[0])
+                        elif len(data_index[0]) == 16 and data_index[2] is not None:
+                            page_data["Leval5"]["data"].append(data_index)
+                    else:
+                        continue
+        return page_data
+
+    def get_time_data(self):
+        """
+        获取表单中存在的有效数据，方便后续筛选的时候使用
+        :return:self.time_data
+        """
+        for company in self.company_sheet_name_dict.keys():
+            for company_date in self.company_sheet_name_dict[company].keys():
+                self.time_data.add(company_date[-4:])
+        self.time_data = list(self.time_data)
+        self.time_data.sort()
+        return self.time_data
+
 
 if __name__ == '__main__':
-    a = FirstDeal("/Users/zhutaohe/Desktop/FInanceTool/经营检测表（费用明细表）数据底稿.xlsx")
+    a = FirstDeal("/Users/MikeImac/Desktop/FinanceTool/经营检测表（费用明细表）数据底稿.xlsx")
